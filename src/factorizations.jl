742802335cc60d4f1ce168c20bc1cd582d6ac3bd
Lukas Devos <ldevos98@gmail.com>
2025-02-06 07:51:16 -0500

    Add truncated svd

--- src/factorizations.jl
+++ src/factorizations.jl
@@ -53,3 +53,30 @@ function svd(a::AbstractArray, biperm::BlockedPermutation{2}; full::Bool=false, 
   axes_Vᴴ = (axes(Vᴴ, 1), axes_domain...)
   return splitdims(U, axes_U), S, splitdims(Vᴴ, axes_Vᴴ)
 end
+
+# TODO: decide on interface
+"""
+    tsvd(A::AbstractArray, labels_A, labels_codomain, labels_domain; kwargs...)
+    tsvd(A::AbstractArray, biperm::BlockedPermutation{2}; kwargs...)
+
+Compute the truncated SVD decomposition of a generic N-dimensional array, by interpreting it
+as a linear map from the domain to the codomain indices. These can be specified either via
+their labels, or directly through a `biperm`.
+"""
+function tsvd(A::AbstractArray, labels_A, labels_codomain, labels_domain; kwargs...)
+  biperm = blockedperm_indexin(Tuple.((labels_A, labels_codomain, labels_domain))...)
+  return tsvd(A, biperm; kwargs...)
+end
+function tsvd(a::AbstractArray, biperm::BlockedPermutation{2}; kwargs...)
+  # tensor to matrix
+  A_mat = fusedims(A, biperm)
+
+  # factorization
+  U, S, Vᴴ = svd_trunc(A_mat; kwargs...)
+
+  # matrix to tensor
+  axes_codomain, axes_domain = blockpermute(axes(A), biperm)
+  axes_U = (axes_codomain..., axes(U, 2))
+  axes_Vᴴ = (axes(Vᴴ, 1), axes_domain...)
+  return splitdims(U, axes_U), S, splitdims(Vᴴ, axes_Vᴴ)
+end
